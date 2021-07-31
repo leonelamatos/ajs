bccca00c6ab9946fc0e1abaf7a531177800c7af1
leonelamatos <leonel.amatos@gmail.com>
2021-07-31 10:00:42 -0400

    Adds SVG package to import SVG files into the project.

--- next.config.js
+++ next.config.js
@@ -1,3 +1,22 @@
 module.exports = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+
+	webpack(config) {
+		config.module.rules.push({
+			test: /\.svg$/,
+			use: ['@svgr/webpack'],
+		});
+
+		return config;
+	},
+};
+module.exports = {
+	webpack(config) {
+		config.module.rules.push({
+			test: /\.svg$/,
+			use: ['@svgr/webpack'],
+		});
+
+		return config;
+	},
+};
