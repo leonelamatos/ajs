49ac50ca364f8cd3ea5bd0ead772ad401b14e73a
leonelamatos <leonel.amatos@gmail.com>
2021-08-13 09:18:37 -0400

    Adds main user home page.

---
+++ pages/user/home.jsx
@@ -0,0 +1,25 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import Header from '../../layouts/header';
+import Sidebard from '../../layouts/Sidebard';
+
+export default function Home() {
+	return (
+		<div className='test'>
+			<Head>
+				<title>Active Job Staus</title>
+				<meta name='description' content='Active job status app' />
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+			<Header />
+			<div className='row'>
+
+
+			<Sidebard />
+			<main>
+				<h1>Main content</h1>
+			</main>
+			</div>
+		</div>
+	);
+}
