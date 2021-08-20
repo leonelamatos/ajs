78a7140e83a2fab50043fc9916dd129892b6583b
leonelamatos <leonel.amatos@gmail.com>
2021-08-20 10:58:14 -0400

    Adds sidebar icons

--- components/layouts/Sidebard.jsx
+++ components/layouts/Sidebard.jsx
@@ -3,7 +3,14 @@ import React from 'react';
 const Sidebard = () => {
 	return (
 		<aside className='aside'>
-			<h2>Sidebar</h2>
+			<div className='aside-container'>
+				<img
+					id='dashboard'
+					src='../dashboard_icon.png'
+					alt='white dashboard icon'
+				/>
+				<img src='../task_icon_white.png' alt='white task icon' />
+			</div>
 		</aside>
 	);
 };
